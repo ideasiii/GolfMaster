7b22d9a13363fd3ceafdb6cc09d156bc00d27aa0
JamesChangChung <james22753114@gmail.com>
2022-12-23 17:40:09 +0800

    註解DeviceData.java重複

--- src/com/golfmaster/moduel/DeviceData.java
+++ src/com/golfmaster/moduel/DeviceData.java
@@ -83,14 +83,14 @@ public abstract class DeviceData
 	// 7.Push Hook, 8.Push, 9.Push Slice
 
 	// TODO: 其他問題球:
-<<<<<<< HEAD
-	public static final String	THE_TOP				= "The Top";			// 1.The
+//<<<<<<< HEAD
+//	public static final String	THE_TOP				= "The Top";			// 1.The
 																			// Top(剃頭球.切滾球)
-	public static final String	DUFF				= "Duff";				// 2.Duff
+//	public static final String	DUFF				= "Duff";				// 2.Duff
 																			// (先擊到球後方之草〈地〉，再擊到球)
-	public static final String	SHANK				= "Shank";				// 3.Shank
+//	public static final String	SHANK				= "Shank";				// 3.Shank
 																			// (棒擊球)
-	public static final String	LACK_OF_STRENGTH	= "Lack of Strength";	// 8.Lack
+//	public static final String	LACK_OF_STRENGTH	= "Lack of Strength";	// 8.Lack
 																			// of
 																			// Strength
 																			// (力道不足)
@@ -101,13 +101,13 @@ public abstract class DeviceData
 	public static final float Putt_FACE_SQUARE_MAX = 4;		
 	
 	
-=======
+//=======
 	public static final String	THE_TOP				= "擊球失誤導致擊出剃頭球或切滾球";			
 	public static final String	DUFF				= "擊球失誤導致先擊到球後方之草地，再擊到球";				
 	public static final String	SHANK				= "擊球失誤導致棒擊球";				
 	public static final String	LACK_OF_STRENGTH	= "力道不足的擊球";
 
->>>>>>> a747b2979cd9caffe21fb9ec4799b6576ff955b3
+//>>>>>>> a747b2979cd9caffe21fb9ec4799b6576ff955b3
 	public class WrgData
 	{
 		public float	BallSpeed;
